e921d8abbb72fed676c259431eb12c49f7383158
MmmarRTHa <m27842n@outlook.com>
2024-04-09 19:37:07 -0700

    - update: feature/register_controller - add register controller

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/create-account', function () {
-    return view('auth.register');
-});
+Route::get('/create-account', [RegisterController::class, 'index']);

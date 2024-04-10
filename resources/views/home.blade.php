443a60c5ca843bbde95fd2f963755f942c438849
MmmarRTHa <m27842n@outlook.com>
2024-04-09 18:53:40 -0700

    - update: feature/home_layout - add navigation and layout styles

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,9 @@
 @extends('layouts.app')
 
 @section('title_name')
-    Home
+    Home title name
 @endsection
 
 @section('content')
-    <h2>Welcome to LaraGram App</h2>
-    <p>This is the home page of LaraGram App.</p>
+    <p>home content</p>
 @endsection

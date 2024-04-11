78478ba2703c2a640d67ce9a15e354f9985cd70b
MmmarRTHa <m27842n@outlook.com>
2024-04-10 17:56:16 -0700

    - update: feature/timeline - add login controller

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,5 +6,22 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('auth.login');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (!auth()->attempt($request->only('email', 'password'))) {
+            return back()->with('status', 'Invalid login details');
+        }
+
+        return redirect()->route('posts.index');
+    }
 }

3532ecacffd252bfc549dd3e51162685dcc978e1
MmmarRTHa <m27842n@outlook.com>
2024-04-13 16:05:47 -0700

    - update: feature/create_posts - add intervention image library

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -2,13 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class ImageController extends Controller
 {
     public function store(Request $request)
     {
         $image = $request->file('file');
-        return response()->json(['image' => $image->extension()]);
+        $imageName = Str::uuid() . '.' . $image->extension();
+
+        $serverImage = Image::make($image);
+        $serverImage->fit(1000, 1000);
+
+        $imagePath = public_path('uploads/' . '/' . $imageName);
+        $serverImage->save($imagePath);
+
+        return response()->json(['image' => $imageName]);
     }
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -11,3 +11,15 @@ const dropzone = new Dropzone('#dropzone', {
     maxFiles: 1,
     uploadMultiple: false,
 });
+
+dropzone.on('sending', function (file, xhr, formData) {
+    console.log(formData);
+});
+
+dropzone.on('success', function (file, response) {
+    console.log(response);
+});
+
+dropzone.on('error', function (file, message) {
+    console.log(message);
+});

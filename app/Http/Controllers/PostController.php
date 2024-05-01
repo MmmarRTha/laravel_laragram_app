af928373168acaa2836a444942345fca4210558e
MmmarRTHa <m27842n@outlook.com>
2024-04-30 22:05:50 -0700

    - update: feature/home - show followers's posts on home page

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,7 @@ class PostController extends Controller
 {
     public function index(User $user)
     {
-        $posts = Post::where('user_id', $user->id)->paginate(12);
+        $posts = Post::where('user_id', $user->id)->latest()->paginate(12);
 
         return view('dashboard', [
             'user' => $user,
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,23 @@
 @extends('layouts.app')
 
 @section('title_name')
-    Home title name
+    Home
 @endsection
 
 @section('content')
-    <p>home content</p>
+    @if ($posts->isEmpty())
+        <p class="text-sm font-bold text-center text-gray-800 uppercase">No posts yet, start following someone :D </p>
+    @endif
+    <div class="grid gap-6 px-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        @foreach ($posts as $post)
+            <div>
+                <a href="{{ route('posts.show', ['post' => $post, 'user' => $post->user]) }}">
+                    <img src="{{ Vite::asset('public/uploads') . '/' . $post->image }}" alt="Image Post" {{ $post->title }}>
+                </a>
+            </div>
+        @endforeach
+    </div>
+    <div class="my-10">
+        {{ $posts->links() }}
+    </div>
 @endsection
